da5c4aa9a54b008d6385ca9a3873c691cad4b356
Alejandro Montes <78356101+alemd01@users.noreply.github.com>
2023-02-24 08:55:38 +0100

    Delete tarea2 directory

--- tarea2/script.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-while ! mysql -u ${USUARIO_BOOKMEDIK} -p${CONTRA_BOOKMEDIK} -h ${DATABASE_HOST}  -e ";" ; do
-        sleep 1
-done
-mysql -u $USUARIO_BOOKMEDIK --password=$CONTRA_BOOKMEDIK -h $DATABASE_HOST $NOMBRE_DB < /var/www/html/schema.sql
-/usr/sbin/apache2ctl -D FOREGROUND
